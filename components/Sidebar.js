08d4b49f0e8e4a938d67946ba39cd741802662cf
SHouvickKoley <sk@shouvick.design>
2022-12-05 23:39:09 -0500

    Created feed componentand its header

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -29,7 +29,7 @@ export default function Sidebar() {
 
         {/* Button */}
 
-        <button className=" bg-green-400 text-white rounded-full w-56 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline">Tweet</button>
+        <button className=" bg-green-400 text-white rounded-full w-56 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline">Zweet</button>
 
 
         {/* Mini-Profile */}
